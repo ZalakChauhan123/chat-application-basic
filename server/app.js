6dbc9be89de3bc750e3f9cb878783b31e302ca5f
zalak-chauhan <chauhanzalak123@gmail.com>
2025-12-14 21:24:37 +0100

    Server Side Seup up Completed

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import { Server } from "socket.io"; // Imp Import
+import { createServer } from 'http'; // Imp Import
+
 const app = express();
-import { Server } from "socket.io";
+const server = createServer(app); // Imp Import
+
+const io = new Server(server);
+
 
 app.get('/' , (req,res) => {
     res.send('Server is Running...');
 });
+
+// Socket.IO
+io.on( 'connection' , ()=> {
+    console.log(`User Connected with socket id ${socket.id}`);
+} )
 
 const port = 3000;
 app.listen( port, () => {
