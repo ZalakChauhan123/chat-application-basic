1ffed7b97fee6a41ca5b9d2a6ef9618279e9f3d4
zalak-chauhan <chauhanzalak123@gmail.com>
2025-12-16 10:32:19 +0100

    One to One Chat Frontend Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,10 +21,13 @@ const App = () => {
 
 
   const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketId] = useState("");
+  const [messages, setMessages] = useState([]); // To Display messages on User display
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit( "message", message );
+    socket.emit( "message", {message, room} );
     setMessage("");
   };
 
@@ -35,13 +38,15 @@ const App = () => {
       // connects to the server. The client emits/handles it locally; 
       // the server doesn't need to create it.
       socket.on( 'connect' , () => {
+        setSocketId(socket.id);
         console.log( `Connected Socket Id ${socket.id}` );
       });
 
       // Event receive : receive-message
       socket.on( 'receive-message' , ( msg ) => {
         console.log( msg );
-      } )
+        setMessages((messages) => [...messages, msg]);
+      });
 
       // Event receive : Welcome
       socket.on( 'welcome', (message) => { console.log(message) } );
@@ -57,13 +62,18 @@ const App = () => {
     <Container maxWidth="sm">
 
 
-      <Box sx={{ height: 500 }} />
-      <Typography variant="h6" component="div" gutterBottom>
+      <Box/>
+      <Typography variant="h3" component="div" gutterBottom>
         Welcome to Socket.io
+      </Typography>
+
+      <Typography variant="h6" component="div" gutterBottom>
+        {socketID}
       </Typography>
 
 
       <form onSubmit={handleSubmit}>
+
         <TextField
           value={message}
           onChange={(e) => setMessage(e.target.value)}
@@ -72,10 +82,41 @@ const App = () => {
           variant="outlined"
         />
 
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+
+      </form>
+
+      <form onSubmit={handleSubmit}>
+
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+        />
 
         <Button type="submit" variant="contained" color="primary">
           Send
         </Button>
+
+        <Stack>
+        {messages.map((m, i) => (
+          <Typography key={i} variant="h6" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+
       </form>
 
     </Container>
