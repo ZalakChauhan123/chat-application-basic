30f4e05196b2423abe318ecc742c8ef64c0a8f92
zalak-chauhan <chauhanzalak123@gmail.com>
2025-12-16 10:52:06 +0100

    Frontendend - Join Group and Messages send functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,9 +20,12 @@ const App = () => {
   );
 
 
+  // One to One Chat useStates
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
   const [socketID, setSocketId] = useState("");
+  // Group Chat useStates
+  const [roomName, setRoomName] = useState("");
   const [messages, setMessages] = useState([]); // To Display messages on User display
 
   const handleSubmit = (e) => {
@@ -30,6 +33,12 @@ const App = () => {
     socket.emit( "message", {message, room} );
     setMessage("");
   };
+
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit( 'join-room', roomName );
+    setRoomName("");
+  }
 
   // How to Handle Events
   useEffect(() => {
@@ -70,6 +79,20 @@ const App = () => {
       <Typography variant="h6" component="div" gutterBottom>
         {socketID}
       </Typography>
+
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+      </form>
 
 
       <form onSubmit={handleSubmit}>
